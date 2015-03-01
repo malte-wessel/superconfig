23be6047ee6b07c9c626ab7abc6b4e3aaaa3d434
Malte W <wessel@rheinschafe.de>
2015-03-01 14:56:21 +0100

    Added tests

--- test/Config.js
+++ test/Config.js
@@ -1,9 +1,56 @@
+/* global __dirname */
 'use strict';
 
 import Config from './../src/Config';
 import test from 'tape';
 
-test('foo', (t) => {
-	t.ok(true, 'bar');
+let p = new Config({
+	path: __dirname + '/config',
+	env: 'production',
+	default: 'production'
+});
+
+let d = new Config({
+	path: __dirname + '/config',
+	env: 'development',
+	default: 'production'
+});
+
+test('Throw error when key undefined', (t) => {
+	t.throws(() => d('notfound'), 'Key notfound not found.', 'Should throw when key undefined');
+	t.throws(() => d('not.found'), 'Key not.found not found.', 'Should throw when nested key undefined');
+	t.end();
+});
+
+test('Get config object', (t) => {
+	t.deepEqual(p('foo'), {foo: 'bar'}, 'Should equals config object');
+	t.end();
+});
+
+test('Get config value', (t) => {
+	t.ok(p('foo.foo') === 'bar', 'Should equals config value');
+	t.end();
+});
+
+test('Get nested config value', (t) => {
+	t.ok(p('nested.bar.foo') === 'bar', 'Should equal nested config value');
+	t.end();
+});
+
+test('Config is immutable', (t) => {
+	let obj = p('foo');
+	obj.foo = 'mutated';
+	t.deepEqual(p('foo'), {foo: 'bar'}, 'Should not be mutated');
+	t.end();
+});
+
+test('Default config', (t) => {
+	t.ok(d('foo.foo') === 'bar', 'Should assigns default values');
+	t.end();
+});
+
+test('Overrides default config', (t) => {
+	t.ok(d('bar.bar') === 'override', 'Should equal development config');
+	t.deepEqual(d('bar.arr'), ['d', 'e'], 'Should equal development config');
 	t.end();
 });
